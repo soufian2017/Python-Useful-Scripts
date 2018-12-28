b687830b166fff3d944d6310c46c06d8c0502f7a
BlackMrx <scrillex@outlook.fr>
2018-12-28 20:34:54 +0100

    new file:   b.py

---
+++ b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from os import system
+
+TMP = "/tmp/test/"
+FILE_NAME = "a.txt"
+
+system("mkdir "+TMP)
+
+system("cat /etc/os-release | grep ID > "+TMP+FILE_NAME)
+
+with open(TMP+FILE_NAME, "r") as f:
+	file = f.read()
+	if "debian" in file:
+		print("Debian")
+	elif "arch" in file:
+		print("Arch")
+	elif "rhel" in file:
+		print("RHEL")
+	else:
+		print("ERROR")
+		
+system("rm "+TMP+FILE_NAME)
+system("rm -r "+TMP)
